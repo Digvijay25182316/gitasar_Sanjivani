e2b921cf57d76ba1daca5547e12ed4725a21c69e
Digvijay25182316 <digvijays.edake2002@gmail.com>
2024-01-31 11:53:44 +0530

    commit

--- src/components/MdLeftHeaderSlider.jsx
+++ src/components/MdLeftHeaderSlider.jsx
@@ -6,6 +6,8 @@ import {
   Bars3BottomLeftIcon,
   CalendarDaysIcon,
   PresentationChartBarIcon,
+  UserGroupIcon,
+  UserIcon,
 } from "@heroicons/react/24/solid";
 import { Link, useLocation } from "react-router-dom";
 
@@ -99,6 +101,34 @@ const Slider = () => {
             >
               <p>
                 <ArrowTrendingUpIcon className="h-6 w-6" />
+              </p>
+              <p>Course Level</p>
+            </div>
+          </Link>
+          <Link to={"/admin/information/volunteers"} onClick={toggleSlider}>
+            <div
+              className={`flex items-center text-lg ${
+                pathname.startsWith("/admin/information/volunteers")
+                  ? "bg-blue-100 text-blue-700 rounded-lg border-r-4 border-r-blue-700 "
+                  : "text-gray-500"
+              }  px-5 py-1.5 mx-2 lg:mx-5 gap-5`}
+            >
+              <p>
+                <UserIcon className="h-6 w-6" />
+              </p>
+              <p>Course Level</p>
+            </div>
+          </Link>
+          <Link to={"/admin/information/participants"} onClick={toggleSlider}>
+            <div
+              className={`flex items-center text-lg ${
+                pathname.startsWith("/admin/information/participants")
+                  ? "bg-blue-100 text-blue-700 rounded-lg border-r-4 border-r-blue-700 "
+                  : "text-gray-500"
+              }  px-5 py-1.5 mx-2 lg:mx-5 gap-5`}
+            >
+              <p>
+                <UserGroupIcon className="h-6 w-6" />
               </p>
               <p>Course Level</p>
             </div>
